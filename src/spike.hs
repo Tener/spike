30bd6aa7fdabbe764962c43c7edd03cd1788fe6a
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-05-15 02:17:53 +0200

    scrolled window, handlers for few signals, show instance for NavigationReason

--- src/spike.hs
+++ src/spike.hs
@@ -2,7 +2,11 @@ module Main where
 
 import Graphics.UI.Gtk.WebKit.WebFrame 
 import Graphics.UI.Gtk.WebKit.WebView
+import Graphics.UI.Gtk.WebKit.Download
 import Graphics.UI.Gtk.WebKit.WebSettings
+import Graphics.UI.Gtk.WebKit.NetworkRequest
+import Graphics.UI.Gtk.WebKit.WebNavigationAction
+
 import Graphics.UI.Gtk
 
 import Data.IORef
@@ -19,6 +23,36 @@ Links:
 
 -}
 
+instance Show NavigationReason where    
+    show WebNavigationReasonLinkClicked	     = "WebNavigationReasonLinkClicked"
+    show WebNavigationReasonFormSubmitted    = "WebNavigationReasonFormSubmitted"
+    show WebNavigationReasonBackForward	     = "WebNavigationReasonBackForward"
+    show WebNavigationReasonReload	     = "WebNavigationReasonReload"
+    show WebNavigationReasonFormResubmitted  = "WebNavigationReasonFormResubmitted"
+    show WebNavigationReasonOther	     = "WebNavigationReasonOther"
+
+-- | install listeners for various signals
+hookupWebView web = do
+  on web downloadRequested $ \ down -> do
+         uri <- downloadGetUri down
+         print ("Download uri",uri)
+         return True
+
+  let foo str webFr netReq webNavAct _webPolDec = do
+                      print str
+                      print =<< webFrameGetName webFr
+                      print =<< webFrameGetUri webFr
+                      print =<< networkRequestGetUri netReq
+                      print =<< webNavigationActionGetReason webNavAct
+                      --print =<< webNavigationActionGetTargetFrame webNavAct
+                      --print (str,t1,t2,t3,t4,t5)
+                      return False
+
+  on web navigationPolicyDecisionRequested $ foo "navigationPolicyDecisionRequested"
+
+--  on web newWindowPolicyDecisionRequested $ foo "newWindowPolicyDecisionRequested"
+
+
 page = do
   -- webkit widget
   web <- webViewNew
@@ -26,14 +60,20 @@ page = do
   let loadHome = webViewLoadUri web "http://google.com"
   loadHome
   webViewSetMaintainsBackForwardList web False
-  print =<< widgetGetSizeRequest web
-  widgetSetSizeRequest web 1240 1024
-  print =<< widgetGetSizeRequest web
+--  print =<< widgetGetSizeRequest web
+--  widgetSetSizeRequest web 1240 1024
+--  print =<< widgetGetSizeRequest web
   
   -- plugins are causing trouble. disable them.
   settings <- webViewGetWebSettings web
   set settings [webSettingsEnablePlugins := False]
       
+  hookupWebView web
+
+  -- scrolled window to enclose the webkit
+  scrollWeb <- scrolledWindowNew Nothing Nothing
+  containerAdd scrollWeb web
+
   -- menu
   menu <- hBoxNew False 1
   quit <- buttonNewWithLabel "Quit"
@@ -48,7 +88,7 @@ page = do
   -- fill the page
   page' <- vBoxNew False 10
   containerAdd page' menu
-  containerAdd page' web
+  containerAdd page' scrollWeb
   
   widgetShowAll page'
   return page'
