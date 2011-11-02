6dfbd128a10f91c1b0b6079b58a2957ba26ff2e0
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-11-03 00:59:52 +0100

    visual browse tree widget, improvements

--- src/spike2.hs
+++ src/spike2.hs
@@ -10,9 +10,11 @@ import Graphics.UI.Gtk.WebKit.WebNavigationAction
 -- import Graphics.UI.Gtk.WebKit.WebWindowFeatures
 
 import System.IO.Unsafe
+import System.Process
+import System.Exit
 
 import Graphics.UI.Gtk
-
+import Text.Printf
 import Control.Monad
 import Control.Concurrent.STM
 import Control.Concurrent
@@ -21,6 +23,8 @@ import qualified Data.Foldable as F
 import qualified Data.Traversable as T
 
 import Utils
+import Data.Tree.Zipper
+import Data.Maybe
 
 -- import "mtl" Control.Monad.Trans
 
@@ -184,7 +188,7 @@ newWeb :: BrowseTreeState -> IO () -> String -> IO (Widget, WebView)
 newWeb btreeSt refreshLayout url = do
   -- webkit widget
   web <- webViewNew
-  webViewSetTransparent web True
+  webViewSetTransparent web False
   let loadHome = webViewLoadUri web url
   loadHome
   -- webViewSetMaintainsBackForwardList web False -- TODO: or maybe True?
@@ -445,6 +449,7 @@ listPagesBox focusOnPage pages box = do
   widgetShowAll box
   return ()
 
+noEntriesInBox :: ContainerClass self => self -> IO ()
 noEntriesInBox box = do
   containerForeach box (containerRemove box)
 --  label <- labelNew (Just "(no elements here)") -- Nothing
@@ -453,19 +458,75 @@ noEntriesInBox box = do
   containerAdd box label
   widgetShowAll box
 
+newScrolledWindowWithViewPort :: WidgetClass child => child -> IO ScrolledWindow
 newScrolledWindowWithViewPort child = do
   sw <- scrolledWindowNew Nothing Nothing
   scrolledWindowSetPolicy sw PolicyAutomatic PolicyAutomatic
   scrolledWindowAddWithViewport sw child
   return sw
 
-browseTreeToDot btree = do
+browseTreeToSVG :: [Tree Page] -> IO String
+browseTreeToSVG btree = do
+  let ellipsis t n | length t < n = t
+                   | otherwise = take n t ++ "..." 
+
+  nodeID <- newTVarIO (0::Int)
+  btree' <- mapM (T.mapM (\p -> do
+            i <- atomically $ do
+                   iden <- readTVar nodeID
+                   writeTVar nodeID (iden+1)
+                   return iden
+            t <- getPageTitle p
+            return (i,t))) btree
+  
+  let prelude = unlines ["digraph \"Browse tree\" {",
+                         "graph [",
+                         "fontname = \"Helvetica-Oblique\",",
+                         "page = 10",
+                         "size = 30", 
+                         " ];"]
+      -- labels = unlines [ printf "d%d [label=\"%s\"];" i t | (i,t) <- concatMap flatten btree' ]
+      footer = "}"
+
+      btreeZip = concatMap flattenToZipper' btree' -- ellipsis t 15
+      labels = unlines [ printf "d%d [URL=\"http://google.com\", shape=polygon, fixedsize=true, fontsize=8, width=1.25, height=0.25, labeltooltip=\"%s\", label=\"%s\"];" 
+                                i t (ellipsis t 10) | (i,t) <- map label btreeZip ]
+      edges = unlines [ printf "d%d -> d%d;" (fst . label . fromJust . parent $ z ) (fst . label $ z) 
+                            | z <- btreeZip, 
+                              parent z /= Nothing]
+
+      edges2 =  [ ((fst . label . fromJust . parent $ z ),(fst . label $ z))
+                      | z <- btreeZip, 
+                             parent z /= Nothing]
+
+
+      everything = prelude ++ edges ++ labels ++ footer
+
+  print everything
+  print edges2
+  tot@(code,svg,dotErr) <- readProcessWithExitCode "dot" ["-Tsvg"] everything
+  _ <- readProcessWithExitCode "dot" ["-Tsvg","-ograph.svg"] everything
+  _ <- readProcessWithExitCode "dot" ["-ograph.dot"] everything
+  print tot
+  case code of
+    ExitSuccess -> return svg
+    ExitFailure c -> return $ printf "<text>Error running 'dot' command. Exit code: %s\n%s</text>" (show c) dotErr
+
+flattenToZipper :: Tree a -> [TreePos Full a]
+flattenToZipper n@(Node _ sub) = fromTree n : concatMap flattenToZipper sub
   
 
+flattenToZipper' :: Tree a -> [TreePos Full a]
+flattenToZipper' n = go (fromTree n) where
+    go z = [z] ++ (fromMaybe [] (fmap go (firstChild z))) ++ (fromMaybe [] (fmap go (next z)))
+
+-- visualBrowseTreeWidget :: t -> IO Widget
+visualBrowseTreeWidget :: TVar [Tree Page] -> IO Widget
 visualBrowseTreeWidget btreeVar = do
   -- webkit widget
   web <- webViewNew
   webViewSetTransparent web True
+  webViewSetFullContentZoom web True
 
   -- scrolled window to enclose the webkit
   scrollWeb <- scrolledWindowNew Nothing Nothing
@@ -473,6 +534,10 @@ visualBrowseTreeWidget btreeVar = do
 
   settings <- webViewGetWebSettings web
   set settings [webSettingsEnablePlugins := False]
+
+  let refreshSVG = do
+        svg <- browseTreeToSVG =<< readTVarIO btreeVar
+        webViewLoadString web svg (Just "image/svg+xml") Nothing ""
   
   on web navigationPolicyDecisionRequested $ \ webframe networkReq webNavAct webPolDec -> do
     print "[navigationPolicyDecisionRequested]"
@@ -484,8 +549,17 @@ visualBrowseTreeWidget btreeVar = do
     return True
 
   -- watch btreeVar for changes, update
+  let watchdog page = do
+          page' <- waitTVarChangeFrom page btreeVar
+          postGUIAsync refreshSVG
+          watchdog page'
+  forkIO (watchdog =<< readTVarIO btreeVar)
+  forkIO (forever $ do
+            threadDelay (10^6)
+            postGUIAsync refreshSVG)
 
-  return scrollWeb
+  refreshSVG
+  return (toWidget scrollWeb)
 
 
 main :: IO ()
@@ -601,6 +675,15 @@ main = do
               windowAllowGrow := True ]
  widgetShowAll window
 
+ -- tree view window
+ window2 <- windowNew
+ visualBT <- visualBrowseTreeWidget btreeVar
+ set window2 [ containerBorderWidth := 10,
+              windowTitle := "Spike browser - visual browse tree",
+              containerChild := visualBT,
+              windowAllowGrow := True ]
+ widgetShowAll window2
+ 
  -- viewPage topPage
  refreshLayout
 
