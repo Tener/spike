9e92f3dc00790ef749f468a989ead4608b78650d
Krzysztof Skrzętnicki <gtener@gmail.com>
2012-02-29 22:01:16 +0100

    add parameters for setting globals

--- cbits/cbits.c
+++ cbits/cbits.c
@@ -7,13 +7,14 @@
 #include <libsoup/soup.h>
 #include <webkit/webkit.h>
 
-void spike_setup_webkit_globals()
+void spike_setup_webkit_globals(char * web_database_path, char * cookie_jar_path)
 {
-  webkit_set_web_database_directory_path("/tmp/spike.webkit.db");
+  //eg. /tmp/spike.webkit.db
+  webkit_set_web_database_directory_path(web_database_path);
 
+  
   SoupSession * ses = webkit_get_default_session();
-  //  SoupSessionFeature * jar = soup_session_get_feature(ses, 
-  SoupCookieJar * jar = soup_cookie_jar_sqlite_new("/tmp/spike.webkit.cookie.db", 0);
+  //eg. /tmp/spike.webkit.cookie.db
+  SoupCookieJar * jar = soup_cookie_jar_sqlite_new(cookie_jar_path, 0);
   soup_session_add_feature( ses, SOUP_SESSION_FEATURE (jar));
-
 }
